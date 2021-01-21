f7a6e8dd0d5c8359b019802badf6aef4992a5932
wdingsoft <wdingbox@gmail.com>
2021-01-20 19:07:23 -0500

    Sync:len:280,dlt:272. repodesc:

---
+++ account/dat/localStorage_json.js
@@ -0,0 +1,8 @@
+var localStorage = {
+    "FontSize": "16",
+    "repositories": "[{\"repopath\":\"https://github.com/bsnp21/pub_test.git\",\"passcode\":\"pub_test\",\"repodesc\":\"\"}]",
+    "#MemoryVerse": "[\"Pro16:9\"]",
+    "test": "1,2",
+    "#RecentMarks": "[\"Pro16:9\"]",
+    "#RecentBooks": "[\"Pro1:1\"]"
+}
