8bfe4c599d6a0c6abf02f66d9e478eb3b9b2b727
wdingsoft <wdingbox@gmail.com>
2021-01-20 19:09:38 -0500

    Sync:len:129,dlt:-151. repodesc:

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,8 +1,3 @@
 var localStorage = {
-    "FontSize": "16",
-    "repositories": "[{\"repopath\":\"https://github.com/bsnp21/pub_test.git\",\"passcode\":\"pub_test\",\"repodesc\":\"\"}]",
-    "#MemoryVerse": "[\"Pro16:9\"]",
-    "test": "1,2",
-    "#RecentMarks": "[\"Pro16:9\"]",
-    "#RecentBooks": "[\"Pro1:1\"]"
+    "repositories": "[{\"repopath\":\"https://github.com/bsnp21/pub_test.git\",\"passcode\":\"pub_test\",\"repodesc\":\"\"}]"
 }
