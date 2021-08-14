d1ae21aa7bda26ac4089d4e38ea5e1a1409e2d44
Ubuntu <ubuntu@ip-172-31-87-112.ec2.internal>
2021-08-14 15:29:41 +0000

    test

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -2,7 +2,7 @@ var localStorage = {
     "FontSize": "16",
     "repositories": "[{\"repopath\":\"https://github.com/bsnp21/pub_test.git\",\"passcode\":\"pub_test\",\"repodesc\":\"\"}]",
     "#MemoryVerse": "[\"Pro16:9\"]",
-    "test": "1,2",
+    "test": "1,2,3",
     "#RecentMarks": "[\"Pro16:9\"]",
     "#RecentBooks": "[\"Pro1:1\"]"
 }
